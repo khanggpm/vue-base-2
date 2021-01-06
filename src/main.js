056e9427af8a7109ff16d7c48efd7818b17d2ef6
Pham Khang <khangphamminh@sohagame.vn>
2021-01-06 09:58:12 +0700

    add first new project

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from './store'
+
+//element ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.config.productionTip = false
+
+Vue.use(ElementUI);
+
+new Vue({
+  ElementUI,
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  routes,
+});
+
+router.beforeResolve((to, from, next) => {
+  // If this isn't an initial page load.
+  if (to.name) {
+    // Start the route progress bar.
+    NProgress.start()
+  }
+  next()
+})
+
+router.afterEach((to, from) => {
+  // Complete the animation of the route progress bar.
+  NProgress.done()
+})
+
+
+export default router;
